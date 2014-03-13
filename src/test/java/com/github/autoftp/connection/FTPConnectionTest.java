c78f5816f2fab433aac6fddb1620e12f0f9a2f1d
JAGFin1 <jagfin1@gmail.com>
2014-03-13 19:25:48 +0000

    Added passing test

--- src/test/java/com/github/autoftp/connection/FTPConnectionTest.java
+++ src/test/java/com/github/autoftp/connection/FTPConnectionTest.java
@@ -1,5 +1,14 @@
 package com.github.autoftp.connection;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
 public class FTPConnectionTest {
 
+	@Test
+	public void passingTest() {
+		
+		assertTrue(true);
+	}
 }
